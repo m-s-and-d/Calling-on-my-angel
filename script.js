c64e3932ec43b4ecf663f8d6c992389ce2f7ce71
m-s-and-d <ciochinacarolina113@gmail.com>
2026-07-24 16:36:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+const button = document.getElementById("enterButton");
 
+const firstScreen = document.querySelector(".project-screen");
+
+const angelScreen = document.querySelector(".angel-screen");
+
+
+button.addEventListener("click", () => {
+
+
+    firstScreen.style.transition = "2s";
+
+    firstScreen.style.opacity = "0";
+
+
+    setTimeout(() => {
+
+
+        firstScreen.style.display = "none";
+
+
+        angelScreen.style.opacity = "1";
+
+        angelScreen.style.pointerEvents = "auto";
+
+
+    }, 2000);
+
+
+});
