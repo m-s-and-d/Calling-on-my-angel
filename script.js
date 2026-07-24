95936a7e3d8f0e5b1fa089094fd0eceaa127016f
m-s-and-d <ciochinacarolina113@gmail.com>
2026-07-24 18:02:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-alert("Hello");
+const button = document.getElementById("enterButton");
+
+const firstScreen = document.querySelector(".project-screen");
+
+const angelScreen = document.querySelector(".angel-screen");
+
+
+button.addEventListener("click", () => {
+
+    firstScreen.style.transition = "2s";
+    firstScreen.style.opacity = "0";
+
+
+    setTimeout(() => {
+
+        firstScreen.style.display = "none";
+
+        angelScreen.style.opacity = "1";
+        angelScreen.style.pointerEvents = "auto";
+
+
+        const angelContent = document.querySelector(".angel-content");
+
+        angelContent.style.opacity = "1";
+        angelContent.style.transform = "translateY(0)";
+
+
+    }, 2000);
+
+});
